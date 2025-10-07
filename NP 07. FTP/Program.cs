98859af796ef4e593bb52e7384eccd29a31e543d
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-10-07 21:49:26 +0400

    Mail

---
+++ NP 07. FTP/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Net;
+
+// Использовали https://sftpcloud.io/tools/free-ftp-server
+// бесплатный demo FTP сервер на час 
+
+//UploadFTP();
+//GetFTP();
+//DownloadFTP();
+void GetFTP() {
+    var request = WebRequest.Create("ftp://eu-central-1.sftpcloud.io:21") as FtpWebRequest;
+    request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+    request.Credentials = new NetworkCredential("abbcf2d7d12f4024ac49f022e776f08f", "QdLz4sqmg17KUxUEtij1Xn4p0QINxK1P");
+    var response = request.GetResponse() as FtpWebResponse;
+    var stream = response.GetResponseStream();
+    var reader = new StreamReader(stream);
+    var data = reader.ReadToEnd();
+    Console.WriteLine(data);
+
+}
+void UploadFTP() {
+    var request = WebRequest.Create("ftp://eu-central-1.sftpcloud.io:21/google.zip") as FtpWebRequest;
+    request.Method = WebRequestMethods.Ftp.UploadFile;
+    request.Credentials = new NetworkCredential("abbcf2d7d12f4024ac49f022e776f08f", "QdLz4sqmg17KUxUEtij1Xn4p0QINxK1P");
+    var fileStream = new FileStream(@"C:\Users\zamanov\Desktop\GoogleChromeEnterpriseBundle64.zip", FileMode.Open);
+    var stream = request.GetRequestStream();
+    fileStream.CopyTo(stream);
+    stream.Close();
+    fileStream.Close();
+}
+void DownloadFTP() {
+    var request = WebRequest.Create("ftp://eu-central-1.sftpcloud.io:21/Salam.txt") as FtpWebRequest;
+    request.Method = WebRequestMethods.Ftp.DownloadFile;
+    request.Credentials = new NetworkCredential("abbcf2d7d12f4024ac49f022e776f08f", "QdLz4sqmg17KUxUEtij1Xn4p0QINxK1P");
+    var response = request.GetResponse() as FtpWebResponse;
+    var stream = response.GetResponseStream();
+
+    var fileStream = new FileStream(@"..\..\Salat.txt", FileMode.Create);
+
+    stream.CopyTo(fileStream);
+    fileStream.Close();
+
+}
---
+++ NP 07. Mail Protocols/Program.cs
@@ -0,0 +1,102 @@
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Net.Smtp;
+using MailKit.Search;
+using MimeKit;
+
+//using System.Net;
+//using System.Net.Mail;
+
+
+
+//SMTP();
+//SMTPMailKit();
+IMAP();
+
+//void SMTP()
+//{
+//    var client = new SmtpClient("smtp.gmail.com", 587);
+//    client.EnableSsl = true;
+
+//    client.Credentials = new NetworkCredential("fbms.1223@gmail.com", "opulnrgguvhhebvt");
+
+//    var message = new MailMessage
+//    {
+//        Subject = "Test Mail Protocols",
+//        Body = "WINTER IS COMING!!! (WINTER=EXAM)"
+//    };
+
+//    message.From = new MailAddress("fbms.1223@gmail.com", "Donald Trump");
+
+//    //message.To.Add(new MailAddress("nailhere849@gmail.com"));
+//    //message.To.Add(new MailAddress("vagif.aliev.17.04@gmail.com"));
+//    //message.To.Add(new MailAddress("ravansafarovelitar@gmail.com"));
+//    //message.To.Add(new MailAddress("alibeylitural212@gmail.com"));
+//    message.To.Add(new MailAddress("zamanov@itstep.org"));
+
+//    client.Send(message);
+//}
+
+
+void SMTPMailKit()
+{
+    var client = new SmtpClient();
+    client.Connect("smtp.gmail.com", 587);
+    client.Authenticate("fbms.1223@gmail.com", "opulnrgguvhhebvt");
+
+    var message = new MimeKit.MimeMessage();
+
+    message.From.Add(new MailboxAddress("Barak Obama", "fbms.1223@gmail.com"));
+
+    message.To.AddRange(new[]
+    {
+        MailboxAddress.Parse("nailhere849@gmail.com"),
+        MailboxAddress.Parse("vagif.aliev.17.04@gmail.com"),
+        MailboxAddress.Parse("ravansafarovelitar@gmail.com"),
+        MailboxAddress.Parse("alibeylitural212@gmail.com"),
+        MailboxAddress.Parse("zamanov@itstep.org")
+    });
+
+    message.Subject = "Exam";
+    //message.Body = new TextPart("plain")
+    //{
+    //    Text = "WINTER IS COMING!!! (WINTER=EXAM)"
+    //};
+
+    message.Body = new TextPart("html")
+    {
+        Text = """
+            <h1>WINTER IS COMING!!! (WINTER=EXAM)</h1>
+            <img src='https://images2.minutemediacdn.com/image/upload/c_fill,w_1440,ar_16:9,f_auto,q_auto,g_auto/shape/cover/sport/WinterIsComing_social-share-7076d8e90f656c73183ce00052be52a4.jpg'/>
+        """
+    };
+
+    client.Send(message);
+    client.Disconnect(true);
+}
+
+void IMAP()
+{
+    var imapClient = new ImapClient();
+    imapClient.Connect("imap.gmail.com", 993, true);
+    imapClient.Authenticate("fbms.1223@gmail.com", "opulnrgguvhhebvt");
+
+    var inbox = imapClient.Inbox;
+    inbox.Open(FolderAccess.ReadWrite);
+
+    var ids = inbox.Search(SearchQuery.All);
+
+    //foreach(var id in ids)
+    //{
+    //    Console.WriteLine($"{id}. {inbox.GetMessage(id).TextBody}");
+    //}
+
+    //inbox.SetFlags(new UniqueId(55), MessageFlags.Deleted, true);
+    //inbox.AddFlags(ids, MessageFlags.Seen, true);
+    inbox.AddFlags(ids, MessageFlags.Deleted, true);
+
+    inbox.Expunge();
+    inbox.Close();
+
+
+}
